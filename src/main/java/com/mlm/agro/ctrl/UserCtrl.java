fd703f7dd2883c806bf662a4d6b14021232f3afe
npati <sumitnayak18>
2022-11-21 22:47:09 +0530

    i have created user ctrl, servbice and dao

---
+++ src/main/java/com/mlm/agro/ctrl/UserCtrl.java
@@ -0,0 +1,17 @@
+package com.mlm.agro.ctrl;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserCtrl {
+	
+	
+	protected void addUser() {
+		
+	}
+	
+	protected void updateUser() {
+		
+	}
+
+}
---
+++ src/main/java/com/mlm/agro/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.mlm.agro.repo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepo {
+
+}
