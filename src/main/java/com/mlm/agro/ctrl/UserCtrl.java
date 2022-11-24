57acd3b74ea7e530494474e79e1ef1549188b2da
sumitnahyak18 <sumitnayak18>
2022-11-24 16:55:46 +0530

    My first commit.

--- src/main/java/com/mlm/agro/ctrl/UserCtrl.java
+++ src/main/java/com/mlm/agro/ctrl/UserCtrl.java
@@ -1,16 +1,26 @@
 package com.mlm.agro.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mlm.agro.entity.UserEntity;
+import com.mlm.agro.service.UserService;
 
 @RestController
 public class UserCtrl {
 	
-	
-	
-	protected void addUser() {
-		
+ @Autowired
+ private UserService service ;
+ 
+ 
+   @PostMapping("/registerUser")
+	protected void addUser(@RequestBody UserEntity entity) {
+	   System.out.println("controller called");
+		service.addUser(entity);
 	}
-	
 	
 	protected void updateUser() {
 		
--- src/main/java/com/mlm/agro/service/UserService.java
+++ src/main/java/com/mlm/agro/service/UserService.java
@@ -1,9 +1,29 @@
 package com.mlm.agro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mlm.agro.entity.UserEntity;
+import com.mlm.agro.repo.UserRepo;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepo repo;
+	
+	
+	
+	
+	public void addUser(UserEntity entity) {
+		System.out.println("service called");
+		repo.existsById(entity.getUserId());
+		repo.findByParentId(entity.getParentId());
+		repo.findByMobileNo(entity.getMobileNo());
+		entity.setReferalLink(null);
+		entity.setStatus("N");
+		repo.save(entity);
+	}
 
 	
 	
